ed841ee10367da5605f938259136985491066800
Igor Šarčević <igor@renderedtext.com>
2017-11-29 22:48:30 +0000

    Accept connections and read their requests

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use std::io::prelude::*;
+use std::net::TcpListener;
+use std::net::TcpStream;
+
 fn main() {
-    println!("Hello, world!");
+    let address = "0.0.0.0:8080";
+    let listener = TcpListener::bind(address).unwrap();
+
+    println!("Listening on {}", address);
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        handle_connection(stream);
+
+    }
+}
+
+fn handle_connection(mut stream : TcpStream) {
+    println!("Connection Established!");
+
+    let mut buffer = [0; 512];
+
+    stream.read(&mut buffer).unwrap();
+
+    println!("Result: {}", String::from_utf8_lossy(&buffer[..]));
 }
