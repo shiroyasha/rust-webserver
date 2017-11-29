d223a0beeced38d45130d00f1a62982eb0d81b09
Igor Šarčević <igor@renderedtext.com>
2017-11-29 23:17:38 +0000

    Simulate a slow response

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
 use std::fs::File;
+use std::thread;
+use std::time::Duration;
 
 fn main() {
     let address = "0.0.0.0:8080";
@@ -27,8 +29,12 @@ fn handle_connection(mut stream : TcpStream) {
     println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
 
     let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
 
     let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
+    } else if buffer.starts_with(sleep) {
+        thread::sleep(Duration::from_secs(5));
         ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
     } else {
         ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
